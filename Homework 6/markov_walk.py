9c06229032e04a9bae65c704f81a2a9f23460a8b
taranoshi <taranoshi@gmail.com>
2018-11-07 10:43:57 -0500

    added the markov walk expected value calculation

--- Homework 6/markov_walk.py
+++ Homework 6/markov_walk.py
@@ -1,6 +1,9 @@
 import numpy as np
 import random
 
+def expected_value(p, x):
+    return sum([pi * xi for pi, xi in zip(p,x)])
+
 transition_matrix = [ [0.5,0.35,0.15,0.], [0.,0.,0.2,0.8], [0.,0.,0.3,0.7], [1.0,0.,0.,0.]]
 state_vector = [0.,1.0,0.,0.]
 super_position_state = np.dot(state_vector, transition_matrix)
@@ -17,7 +20,7 @@ def get_next_state(super_position_state):
         i += 1
          
     new_state = [0.] * len(super_position_state)
-    new_state[random.choice(state_list)] = 1.0;
+    new_state[random.choice(state_list)] = 1.0
      
     return new_state
 
@@ -27,7 +30,25 @@ def get_state(state_vector):
     index = [i for i, e in enumerate(state_vector) if e != 0]
     return 'S' + str(index[0])
 
-for i in range(100):
+posCounts = {}
+steps = 100
+for i in range(steps):
     super_position_state = np.dot(state_vector, transition_matrix)
     state_vector = get_next_state(super_position_state)
     print(state_vector, ':', get_state(state_vector))
+    s = get_state(state_vector)
+    if s in posCounts.keys():
+        posCounts[s] += 1
+    else:
+        posCounts[s] = 1
+posCountsK = posCounts.keys()
+for k in posCountsK:
+    posCounts[k] = posCounts[k] / steps
+lbls = []
+lbl = 0
+for k in posCountsK:
+    lbls.append(lbl)
+    lbl += 1
+posCountsK = list(posCountsK)
+#print(expected_value(posCounts.values(), lbls))
+print("The expected value is state: {0}".format(posCountsK[int(expected_value(posCounts.values(), lbls))]))
